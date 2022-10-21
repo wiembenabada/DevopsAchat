5daf9e252a66147a3f8abd5f3900f0680e075e36
Ghofran.Ghanmi <ghofran.ghanmi@esprit.tn>
2022-10-21 20:32:41 +0100

    ghofran commit

--- src/main/java/tn/esprit/rh/achat/AchatApplication.java
+++ src/main/java/tn/esprit/rh/achat/AchatApplication.java
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 public class AchatApplication {
 
+/**** */
     public static void main(String[] args) {
         SpringApplication.run(AchatApplication.class, args);
     }
